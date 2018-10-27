51a07f7ef3ab742b9f98ca62707048088d42ed81
gichulkim <gichulkim@192.168.0.3>
2018-10-27 11:13:40 +0900

    Task3

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -60,9 +60,10 @@ public class CLAMITest {
 		// TASK3: Conduct defect prediction by using CLAMI models
 		// complete this test case for the generated arff files from Task 2
 		
-		//String[] args = {"-f","data/[your_generated_metric_data].arff","-l","label","-p", "buggy", "-m"};
-		
-		//CLAMI.main(args);
+		String[] args = {"-f","data/PolyMorphism.arff","-l","label","-p", "buggy", "-m"};
+		String[] args2 = {"-f","data/InheritanceExample.arff","-l","label","-p", "buggy", "-m"};
+		CLAMI.main(args);
+		CLAMI.main(args2);
 	}
 	
 	@Test
